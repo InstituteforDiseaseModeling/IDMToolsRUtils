07404f196cbbd07a66e19da252011480303e95f9
Clinton Collins <clinton.collins@gmail.com>
2021-05-27 19:28:25 -0700

    Export functions

--- R/hello.R
+++ R/hello.R
@@ -45,6 +45,7 @@ get_path_sep <- function() {
 #'
 #' @return Path of the file. Possibly returns NULL when stop_if_not_found is FALSE and the file is not found
 #' @note Possible of a stopping error when stop_if_not_found is TRUE and the file is not found
+#' @export
 find_file_on_env_path <- function(filename, environment_var = "PATH", stop_if_not_found = FALSE) {
   # Get our path seperator. This is OS based. On Windows, we split by ; and : on most other platforms
   path_sep <- get_path_sep()
@@ -79,6 +80,7 @@ find_file_on_env_path <- function(filename, environment_var = "PATH", stop_if_no
 #' @examples
 #' source_file_on_env_path(file.path("R", "make_results_summary.R"))
 #' source_file_on_env_path(file.path("example.R"))
+#' @export
 source_file_on_env_path <- function(filename, environment_var = "PATH") {
   source(find_file_on_env_path(filename, environment_var, TRUE))
 }
